fc008d18d11ff86f92f1b572e405233ed2916b76
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-04-20 13:33:51 -0300

    adicionando o landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/Global";
 import { theme } from "./styles/theme/theme";
-import Landing from "./page";
+import Landing from "./page/Landing";
 
 function App() {
   return (

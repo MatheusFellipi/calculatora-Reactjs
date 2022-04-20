3e14af02fe29a2549a9d109b44e53d94dff1e270
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-04-20 13:35:37 -0300

    Criando o componentes de botao

---
+++ src/components/Buttons/index.tsx
@@ -0,0 +1,30 @@
+import { MouseEvent } from "react";
+import { CButtons } from "./styles";
+
+interface IPropsButtons {
+  name: string;
+  type: "button" | "submit" | "reset";
+  value: string | number;
+  handleClick: (e: MouseEvent<HTMLButtonElement>) => void;
+  visibility?: string;
+}
+
+export function Buttons({
+  name,
+  type,
+  value,
+  handleClick,
+  visibility,
+}: IPropsButtons) {
+  return (
+    <CButtons
+      name={name}
+      type={type}
+      onClick={handleClick}
+      visibility={visibility}
+      value={value}
+    >
+      {name}
+    </CButtons>
+  );
+}

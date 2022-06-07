d342749af24806448bc6c066565fec7f49c1f0c8
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-07 20:49:05 -0300

    Melhorando os estilo do botão

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -1,31 +1,53 @@
-import { MouseEvent } from "react";
-import { CButtons } from "./styles";
+import styled from "styled-components";
 
 interface IPropsButtons {
   name: string;
   type: "button" | "submit" | "reset";
-  value: string | number;
-  handleClick: (e: MouseEvent<HTMLButtonElement>) => void;
+  handleClick?: any;
   visibility?: string;
   color?: string;
+  isInput?: boolean;
 }
 
 export function Buttons({
   name,
   type,
-  value,
   handleClick,
   visibility,
 }: IPropsButtons) {
   return (
-    <CButtons
-      name={name}
-      type={type}
-      onClick={handleClick}
-      visibility={visibility}
-      value={value}
-    >
-      {name}
-    </CButtons>
+    <Container>
+      <Button
+        name={name}
+        type={type}
+        onClick={handleClick}
+        visibility={visibility}
+      >
+        {name}
+      </Button>
+    </Container>
   );
 }
+
+type ButtonsProps = {
+  readonly visibility?: string;
+  readonly fontWeight?: string;
+};
+
+const Container = styled.div`
+  width: 6.5rem;
+  height: 6.5rem;
+`;
+
+const Button = styled.button<ButtonsProps>`
+  border-radius: 10px;
+  width: 100%;
+  height: 100%;
+  border-radius: 10rem;
+  border: none;
+  font-size: 2rem;
+  visibility: ${(props) => props.visibility};
+  font-weight: ${(props) => props.fontWeight};
+  background-color: ${(props) => props.theme.palette.primary.main};
+  color: ${(props) => props.theme.palette.primary.contrastText};
+`;

6758c32c5074bf30ada7d4eee07e03361f958e30
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-25 17:28:07 -0300

    Adicionando os temas.

--- src/page/Landing/styles.ts
+++ src/page/Landing/styles.ts
@@ -30,22 +30,26 @@ export const GroupButtonTheme = styled.div`
 export const Calculador = styled.main`
   display: flex;
   flex-direction: column;
-  background: ${(props) => props.theme.palette.common.black};
   width: 25rem;
+  background: ${(props) => props.theme.palette.common.background};
 `;
 
-export const Display = styled.div`
+type DisplayProps = {
+  readonly results?: any;
+};
+
+export const Display = styled.div<DisplayProps>`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  color: white;
+  color: ${(props) => props.theme.palette.text.primary};
   width: 100%;
   height: 120px;
   margin: 2rem 0rem 2rem 0.8rem;
   p {
     max-width: 96%;
     overflow: auto;
-    font-size: 30px;
+    font-size: ${({ results }) => (results !== "" ? "15px" : "30px")};
     text-align: end;
     padding: 0rem 1rem 0 0;
     border-radius: 10px;
